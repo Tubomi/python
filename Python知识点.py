a446b5647661ebf044dcd66089caeb8a161b2d59
Tubomi <41150141+Tubomi@users.noreply.github.com>
2019-02-18 11:58:19 +0800

    Update Python知识点.py

--- Python知识点.py
+++ Python知识点.py
@@ -1,6 +1,7 @@
 Requests 的一些高级特性（Session,stream=True）
 http://docs.python-requests.org/zh_CN/latest/user/advanced.html
-计算程序运行时间的四种方法：https://blog.csdn.net/asialee_bird/article/details/79673860
+在selenium中设置代理ip：https://www.cnblogs.com/nail/p/6616417.html
+计算程序运行时间的四种计算方法：https://blog.csdn.net/asialee_bird/article/details/79673860
  Python random模块sample、randint、shuffle、choice随机函数概念和应用(https://www.cnblogs.com/dylancao/p/8202888.html)
  
  词云：https://www.jianshu.com/p/ead991a08563 按照空格来分图
