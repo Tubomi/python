7f3928d535258741effdaf39f19ff679c76d7623
Tubomi <41150141+Tubomi@users.noreply.github.com>
2019-02-18 20:11:13 +0800

    Update Python知识点.py

--- Python知识点.py
+++ Python知识点.py
@@ -1,12 +1,15 @@
 Requests 的一些高级特性（Session,stream=True）
 http://docs.python-requests.org/zh_CN/latest/user/advanced.html
-在selenium中设置代理ip：1.https://www.cnblogs.com/nail/p/6616417.html 2.https://blog.csdn.net/zwq912318834/article/details/78626739
-计算程序运行时间的四种计算方法：https://blog.csdn.net/asialee_bird/article/details/79673860
- Python random模块sample、randint、shuffle、choice随机函数概念和应用(https://www.cnblogs.com/dylancao/p/8202888.html)
- 
- 词云：https://www.jianshu.com/p/ead991a08563 按照空格来分图
-
+在selenium中设置代理ip：1.https://www.cnblogs.com/nail/p/6616417.html
 selenium+python设置爬虫代理IP：https://blog.csdn.net/zwq912318834/article/details/78626739
+Selenium
+官网 http://www.seleniumhq.org/
+GitHub https://github.com/SeleniumHQ/selenium
+文档 http://selenium-python.readthedocs.io/
+计算程序运行时间的四种计算方法：https://blog.csdn.net/asialee_bird/article/details/79673860
+Python random模块sample、randint、shuffle、choice随机函数概念和应用(https://www.cnblogs.com/dylancao/p/8202888.html)
+词云和jieba:https://blog.csdn.net/fontthrone/article/details/72782971
+
 
 爬虫流处理：https://www.jianshu.com/p/0eff5fb281c0
 r = requests.get('https://pvp.qq.com/web201605/js/herolist.json', stream=True)
