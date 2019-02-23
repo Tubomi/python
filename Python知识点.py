874ba9e60defa41cbb46607ac73c32db6a36fc37
Tubomi <41150141+Tubomi@users.noreply.github.com>
2019-02-23 15:43:31 +0800

    Update Python知识点.py

--- Python知识点.py
+++ Python知识点.py
@@ -1,5 +1,5 @@
 Requests 的一些高级特性（Session,stream=True）
-[^...] 不在[]中的字符：[^abc] 匹配除了a,b,c之外的字符。
+正则：[^...] 不在[]中的字符：[^abc] 匹配除了a,b,c之外的字符。
 http://docs.python-requests.org/zh_CN/latest/user/advanced.html
 在selenium中设置代理ip：1.https://www.cnblogs.com/nail/p/6616417.html
 selenium+python设置爬虫代理IP：https://blog.csdn.net/zwq912318834/article/details/78626739
@@ -9,7 +9,8 @@ Selenium
 GitHub https://github.com/SeleniumHQ/selenium
 文档 http://selenium-python.readthedocs.io/
 中文文档：https://selenium-python-zh.readthedocs.io/en/latest/index.html
-    
+one-hot:
+    https://blog.csdn.net/dongyanwen6036/article/details/78555163
 拉勾获取页面例子：https://www.jianshu.com/p/61dfbc3cd683
 计算程序运行时间的四种计算方法：https://blog.csdn.net/asialee_bird/article/details/79673860
 Python random模块sample、randint、shuffle、choice随机函数概念和应用(https://www.cnblogs.com/dylancao/p/8202888.html)
